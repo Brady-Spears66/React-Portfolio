b1fafc75fbf472d65e00c1a7d93fadfaacdf960b
Brady Spears <spearsy00@gmail.com>
2025-01-22 00:43:09 -0500

    Testing which account I am using

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import logo from './images/mobile-logo.png'
 
-
+// Test comment
 const theme = createTheme({});
 
 const DarkTheme: ThemeOptions = createTheme({
