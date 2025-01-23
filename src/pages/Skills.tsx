733ffbe1b17671732f5120980b1316139304cd31
Brady Spears <spearsy00@gmail.com>
2025-01-23 02:08:11 -0500

    Adding labels to each BallCanvas when hovered

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,44 +1,75 @@
 import { Stack, Typography, useTheme } from "@mui/material";
 import Grid from '@mui/material/Grid2';
-import { Helmet } from "react-helmet-async";
+import React, { useState } from 'react';
 import { languages, librariesFrameworks, tools} from "../constants";
 import { BallCanvas } from "../components/canvas";
 
-export function Skills() {
-    const theme = useTheme();
+// @ts-ignore
+const TechItem = React.memo(({ technology }) => {
+   const [isHovered, setIsHovered] = useState(false);
 
-    return (
-        <Stack direction={'column'} spacing={2} justifyContent={'center'} alignItems={'center'} sx={{width: '100%'}}>
-            <Typography variant={'h3'} sx={{padding: 2}}>Languages</Typography>
-            <Grid container spacing={6} justifyContent="center" sx={{width: '100%'}}>
-                {languages.map((technology) => (
-                    <Grid  size={{ lg: 2}} justifyContent="center" alignItems="center" key={technology.name}>
-                        <div className='w-35 h-35'>
-                            <BallCanvas icon={technology.icon} />
-                        </div>
-                    </Grid>
-                ))}
-            </Grid>
-            <Typography variant={'h3'} sx={{padding: 2}}>Libraries / Frameworks</Typography>
-            <Grid container spacing={6} justifyContent="center" sx={{width: '100%'}}>
-                {librariesFrameworks.map((technology) => (
-                    <Grid  size={{ lg: 2}} justifyContent="center" alignItems="center" key={technology.name}>
-                        <div className='w-35 h-35'>
-                            <BallCanvas icon={technology.icon} />
-                        </div>
-                    </Grid>
-                ))}
-            </Grid>
-            <Typography variant={'h3'} sx={{padding: 2}}>Tools</Typography>
-            <Grid container spacing={6} justifyContent="center" sx={{width: '100%'}}>
-                {tools.map((technology) => (
-                    <Grid  size={{ lg: 2}} justifyContent="center" alignItems="center" key={technology.name}>
-                        <div className='w-35 h-35'>
-                            <BallCanvas icon={technology.icon} />
-                        </div>
-                    </Grid>
-                ))}
-            </Grid>
-        </Stack>
-    );
+   return (
+       <Grid 
+           size={{ lg: 2}} 
+           justifyContent="center" 
+           alignItems="center" 
+           sx={{ 
+               position: 'relative', 
+               textAlign: 'center', 
+               height: '25vh',  // Provide fixed height to accommodate BallCanvas and Typography
+               display: 'flex', 
+               flexDirection: 'column', 
+               alignItems: 'center' 
+           }}
+           onMouseEnter={() => setIsHovered(true)}
+           onMouseLeave={() => setIsHovered(false)}
+       >
+           <div className='w-35 h-35'>
+               <BallCanvas icon={technology.icon} />
+           </div>
+           {isHovered && (
+               <Typography 
+                   variant="body1" 
+                   sx={{
+                       mt: 1,
+                       opacity: isHovered ? 1 : 0,
+                       transition: 'opacity 0.3s ease-in-out',
+                   }}
+               >
+                   {technology.name}
+               </Typography>
+           )}
+       </Grid>
+   );
+});
+
+export function Skills() {
+   const theme = useTheme();
+
+   // @ts-ignore
+   const renderTechSection = (title, technologies) => (
+       <>
+           <Typography variant={'h3'} sx={{padding: 2}}>{title}</Typography>
+           <Grid container spacing={6} justifyContent="center" sx={{width: '100%'}}>
+               {technologies.map((
+                // @ts-ignore
+                technology
+                ) => (
+                   <TechItem 
+                       key={technology.name} 
+                       // @ts-ignore
+                       technology={technology} 
+                   />
+               ))}
+           </Grid>
+       </>
+   );
+
+   return (
+       <Stack direction={'column'} spacing={2} justifyContent={'center'} alignItems={'center'} sx={{width: '100%'}}>
+           {renderTechSection('Languages', languages)}
+           {renderTechSection('Libraries / Frameworks', librariesFrameworks)}
+           {renderTechSection('Tools', tools)}
+       </Stack>
+   );
 }
