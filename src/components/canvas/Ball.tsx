bde46470dd13e5901ff2a40b5a37d59f45307344
Brady Spears <spearsy00@gmail.com>
2025-01-23 02:28:26 -0500

    Adjusting Ball.tsx to separate lights from the Ball mesh

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,12 +1,6 @@
 import React, { Suspense, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import {
-  Decal,
-  Float,
-  OrbitControls,
-  Preload,
-  useTexture,
-} from "@react-three/drei";
+import { Decal, Float, Preload, useTexture } from "@react-three/drei";
 
 import './styles.css';
 
@@ -17,14 +11,27 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
   const meshRef = useRef();
 
+  // Rotate the ball manually while it floats
+  useFrame(() => {
+    if (meshRef.current) {
+      // @ts-ignore
+      meshRef.current.rotation.y += 0.01; // Adjust speed of rotation as needed
+      // @ts-ignore
+    }
+  });
+
   return (
-    <Float speed={15} rotationIntensity={0.2} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+    <Float speed={2} rotationIntensity={0.5} floatIntensity={2}>
+      <mesh 
+        // @ts-ignore
+        ref={meshRef} 
+        castShadow 
+        receiveShadow 
+        scale={2.75}
+      >
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#fff8eb'
+          color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
@@ -51,18 +58,29 @@ const BallCanvas = ({ icon }) => {
   return (
     <div className="ball-container">
       <Canvas
-        frameloop='demand'
+        frameloop="always"
         dpr={[1, 2]}
         gl={{ preserveDrawingBuffer: true }}
         camera={{ position: [0, 0, 10], fov: 50 }}
       >
+        {/* Independent stationary lights */}
+        <ambientLight intensity={0.1} />
+        <directionalLight
+          position={[5, 5, 5]}
+          intensity={0.5}
+          castShadow
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+        />
+        <directionalLight
+          position={[-5, 5, -5]}
+          intensity={0.5}
+          castShadow
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+        />
+
         <Suspense fallback={<CanvasLoader />}>
-          <OrbitControls
-            enableZoom={false}
-            enablePan={false}
-            autoRotate={true}
-            autoRotateSpeed={5}
-          />
           <Ball imgUrl={icon} />
         </Suspense>
         <Preload all />
