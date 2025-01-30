708a343d470b8b77ddeb4502df5e6baadeb1475d
Brady Spears <spearsy00@gmail.com>
2025-01-30 13:10:21 -0500

    Initial commit

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -3,6 +3,9 @@ import { Helmet } from "react-helmet-async";
 export function Education() {
     return (
         <>
+            <h1>
+                Hello World!
+            </h1>
         </>
     )
 }
